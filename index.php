85a5ed02b3a0b17a48a1d5aa83899b93c8de8e7c
ronni <ronnidrum@gmail.com>
2020-10-29 20:56:47 -0300

    tste

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function () {
+
+	$slq = new \Hcode\DB\Sql();
+	$result = $slq->select("select * from tb_users");
+	echo json_encode($result);
+});
+
+$app->run();
